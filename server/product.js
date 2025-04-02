04e054be5b551a10f0450b5b152ec810219fef34
JMStrong55 <147786299+JMStrong55@users.noreply.github.com>
2025-04-01 20:30:40 -0500

    Update product.js

--- server/product.js
+++ server/product.js
@@ -25,36 +25,37 @@ function addProduct(Name, Price, Quantity) {
   //Tests if Name is a string, if not then sends a message
   if (typeof Name === 'string' || Name instanceof String) {
     if (Name === "" || Name === " " || Name === null) {
-      console.log("Please enter a valid name for your product.");
-      return;
+      console.log('Please enter a valid name for your product.');
+      return 'Please enter a valid name for your product.';
     }
   }
   else {
-    console.log("Please enter a valid name for your product.");
-    return;
+    console.log('Please enter a valid name for your product.');
+    return 'Please enter a valid name for your product.';
   }
   
   //Tests if Price is a number, if not then sends a message
   if (typeof Price !== "number" || Price < 0 || !Number.isFinite(Price) || Price == null) {
-    console.log("Please enter a valid price for your product.");
-    return;
+    console.log('Please enter a valid price for your product.');
+    return 'Please enter a valid price for your product.';
   }
   
   //Tests if Quantity is a whole number, if not then sends a message
   if (!Number.isInteger(Quantity) || Quantity === null) {
-    console.log("Please enter a valid quantity for your product.");
-    return;
+    console.log('Please enter a valid quantity for your product.');
+    return 'Please enter a valid quantity for your product.';
   }
   
   productList.push(new product(Name, Price, Quantity));
-  console.log("Success!");
+  console.log('Success!');
+  return 'Success!';
 }
 
 //Searches for products by keyword in the name
 function productSearch(keyword) {
   if (keyword === "" || keyword === " " || keyword === null) {
-      console.log("Please enter a valid keyword.");
-      return;
+      console.log('Please enter a valid keyword.');
+      return 'Please enter a valid keyword.';
   }
   const results = productList.filter(product => 
     product.getName().toLowerCase().includes(keyword.toLowerCase())
@@ -67,5 +68,6 @@ function productSearch(keyword) {
     });
   } else {
     console.log('No products match your search.');
+    return 'No products match your search.';
   }
 }
