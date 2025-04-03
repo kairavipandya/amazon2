c8f204b17c7d9983fce90757ef8aa2aedc689807
KareemOmran123 <koko7sky@gmail.com>
2025-04-03 01:02:57 -0500

    update logout.js
    created function for valid case

---
+++ server/logout.js
@@ -0,0 +1,7 @@
+//logout.js
+
+function logout(logoutClicked, confirmationClicked) {
+    if (logoutClicked === "Valid" && confirmationClicked === "Valid") {
+        return "Logout confirmation page";
+    }
+}
