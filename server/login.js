8d18b1290f493a8b53f6fbd9427428011ace5106
KareemOmran123 <koko7sky@gmail.com>
2025-04-02 23:14:08 -0500

    function password

--- server/login.js
+++ server/login.js
@@ -4,3 +4,15 @@ function isValidUsername(username) {
     if (typeof username !== "string") return false;
     return username.length > 4 && /^[a-zA-Z0-9]+$/.test(username);
 }
+
+function isValidPassword(password) {
+    if (typeof password !== "string") return false;
+    if (password.length <=8) return false;
+
+    const hasUpper = /[A-Z]/.test(password);
+    const hasLower = /[a-z]/.test(password);
+    const hasDigit = /\d/.test(password);
+    const hasSpecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    return hasUpper && hasLower && hasDigit && hasSpecial;
+}
