185e022ecc61989d40c157f26caa155fa5625cd4
KareemOmran123 <koko7sky@gmail.com>
2025-04-03 01:05:30 -0500

    update logout.js
    created if statement for if logout clicked but confirmation invalid

--- server/logout.js
+++ server/logout.js
@@ -4,4 +4,8 @@ function logout(logoutClicked, confirmationClicked) {
     if (logoutClicked === "Valid" && confirmationClicked === "Valid") {
         return "Logout confirmation page";
     }
+
+    if (logoutClicked === "Valid" && confirmationClicked === "Invalid") {
+        return "Profile Dashboard page";
+    }
 }
